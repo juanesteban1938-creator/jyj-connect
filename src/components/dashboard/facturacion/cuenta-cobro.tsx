11e13061d5262e9bf81b14f7f00916049e8b7d59
Juan Esteban Ovalle <juanesteban1938@gmail.com>
2026-01-07 18:56:50 +0000

    ahora se ve gigante, necesito que la pongas en un tamaño proporcional y

--- src/components/dashboard/facturacion/cuenta-cobro.tsx
+++ src/components/dashboard/facturacion/cuenta-cobro.tsx
@@ -158,7 +158,7 @@ export function CuentaCobro({ servicio }: Props) {
                 
                 <div className="mt-8 flex justify-between items-end">
                     <div>
-                         <Image src="https://i.ibb.co/qFPM4pf4/firma.png" alt="Firma Juan Esteban Ovalle" width={256} height={80} objectFit="contain" />
+                         <Image src="https://i.ibb.co/qFPM4pf4/firma.png" alt="Firma Juan Esteban Ovalle" width={180} height={50} objectFit="contain" />
                         <div className="border-t-2 border-black w-64 pt-1">
                              <p className="font-bold">JUAN ESTEBAN OVALLE PINEDA</p>
                             <p className="font-bold">C.C. Ó NIT: 1023940641</p>
