728c259a108c81d3f0f1befb6c4478f5c40bb09d
Juan Esteban Ovalle <juanesteban1938@gmail.com>
2026-03-10 17:20:46 +0000

    Reemplaza el contenido completo de src/firebase/config.ts con esto exact

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,13 +1,9 @@
-/**
- * J&J CONNECT V2.0 - Firebase Config
- * Proyecto: jj-connect--18988325-5ab9e
- * Sincronización verificada con el entorno de producción.
- */
 export const firebaseConfig = {
-  projectId: 'jj-connect--18988325-5ab9e',
-  appId: '1:113157243545:web:dceb7097ee6f367824ee08',
-  apiKey: 'AIzaSyCy0qhbtoA-Xo4w9o47_H12ITUUumh5fkI',
-  authDomain: 'jj-connect--18988325-5ab9e.firebaseapp.com',
-  measurementId: "",
-  messagingSenderId: '113157243545'
+  apiKey: "AIzaSyC-WmAWlqn1jaQF477p0a9hzqsUvSDrKEE",
+  authDomain: "jj-connect--18988325-5ab9e.firebaseapp.com",
+  databaseURL: "https://jj-connect--18988325-5ab9e-default-rtdb.firebaseio.com",
+  projectId: "jj-connect--18988325-5ab9e",
+  storageBucket: "jj-connect--18988325-5ab9e.firebasestorage.app",
+  messagingSenderId: "1093249922175",
+  appId: "1:1093249922175:web:50f30f6ec5b80ed326fb4a"
 };
